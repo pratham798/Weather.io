3e7ca348a4638fe165b2da4d62d4898816d61d1d
Pratham Jamnal <prathamjamnal7982@gmail.com>
2023-04-28 18:53:57 +0530

    Created a Weather App with Rapid Api and Unsplash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import "./Styles/app.scss";
+import axios from "axios";
 
 function App() {
+  const [lon, setLon] = useState("");
+  const [lat, setLat] = useState("");
+  const navigate = useNavigate();
+
+  const getWeather = (lon, lat) => {
+    const options = {
+      method: "GET",
+      url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
+      params: { lon, lat },
+      headers: {
+        "X-RapidAPI-Key": "9ae629c33bmshc19d4d888dc13ffp19d43fjsn37540565e161",
+        "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+      },
+    };
+    axios
+      .request(options)
+      .then((response) => {
+        navigate("/Show-Weather", { state: response.data });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main">
+      <h1>
+        Weather.<span>io</span>
+      </h1>
+      <div className="form-container">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            console.log("Hey");
+            e.preventDefault();
+            getWeather(lon, lat);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="form-group">
+            <label>Enter Longitude in Degrees</label>
+            <br></br>
+            <input
+              type="text"
+              value={lon}
+              placeholder="Enter Longitude"
+              onChange={(e) => {
+                setLon(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label> Enter Latitude in Degrees </label>
+            <br></br>
+            <input
+              type="text"
+              placeholder="Enter Latitude"
+              value={lat}
+              onChange={(e) => {
+                setLat(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit">Enter</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import ShowWeather from "./ShowWeather";
+import App from "./App";
+import { Helmet } from "react-helmet";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Helmet>
+      <title>Weather.io</title>
+    </Helmet>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Show-Weather" element={<ShowWeather />} />
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
